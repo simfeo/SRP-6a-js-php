bb8057a5b812da6fcb2001c41dac4010fe0768ae
simfeo <simidimus@gmail.com>
2022-07-07 00:40:56 +0300

    liitle polishing for signed in page

--- SRP-6A/main.php
+++ SRP-6A/main.php
@@ -25,10 +25,26 @@ if (!isLoggedIn())
 
     <link href="./css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <meta name="theme-color" content="#712cf9">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        .footer_bottom {
+            position: fixed;
+            width: 100%;
+            left: 0;
+            bottom: 0;
+            flex: 0;
+        }
+    </style>
+    <link href="./css/signin.css" rel="stylesheet">
+
 </head>
 
 <body class="text-center">
-    <p>This is an example of a simple HTML page with one paragraph.</p>
+    <h1 "text-center">You have sucessfully logged in.</h1>
+    <footer class="my-3 footer_bottom">
+        <p class="text-center text-muted">&copy;idimus <?php echo date("Y"); ?></p>
+    </footer>
 </body>
 
 </html>
